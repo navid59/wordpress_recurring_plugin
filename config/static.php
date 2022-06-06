bf55065b1fe002d1eb8dcf2112ebebd29338993e
Navid <navid@netopia-system.com>
2022-06-06 05:44:06 +0300

    Update URL

--- config/static.php
+++ config/static.php
@@ -1,9 +1,9 @@
 <?php 
-// const BASE_URL_RECURRING_API_LIVE       = "https://recurring-api-fqvtst6pfa-ew.a.run.app/api/v1/";
-// const BASE_URL_RECURRING_API_SANDBOX    = "https://recurring-api-fqvtst6pfa-ew.a.run.app/api/v1/";
+const BASE_URL_RECURRING_API_LIVE       = "https://recurring-api-fqvtst6pfa-ew.a.run.app/api/v1/";
+const BASE_URL_RECURRING_API_SANDBOX    = "https://recurring-api-fqvtst6pfa-ew.a.run.app/api/v1/";
 
-const BASE_URL_RECURRING_API_SANDBOX = "http://localhost:8080/api/v1/";
-const BASE_URL_RECURRING_API_LIVE = "http://localhost:8080/api/v1/";
+//const BASE_URL_RECURRING_API_SANDBOX = "http://localhost:8080/api/v1/";
+//const BASE_URL_RECURRING_API_LIVE = "http://localhost:8080/api/v1/";
 
 const URL_NETOPIA_PAYMENTS              = "https://netopia-payments.com/";
 const URL_NETOPIA_PAYMENTS_REGISTRATION = "https://netopia-payments.com/register/";
