f59affa346fab4f0a0f74c84564e022d6b80b395
Navid <navid@netopia-system.com>
2022-06-08 19:05:57 +0300

    update

--- wc-netopiapayments-recurring-notify.php
+++ wc-netopiapayments-recurring-notify.php
@@ -65,6 +65,9 @@ function get3DSAuthorizeRedirect() {
 function getHeaderRequest() {
     global $wpdb;
     $obj = new recurring();
+    /** Log Durring Implimentare*/
+    (new DumpHTTPRequestToFile)->execute(WP_PLUGIN_DIR . '/netopia-recurring/log/notifyUrl.txt');
+    
     /** Log Time & Path*/
     $logDate = new DateTime();
     $logDate = $logDate->format("y:m:d h:i:s");
@@ -142,3 +145,57 @@ function isValidToken($apiKey) {
         return false;
     }
 }
+
+
+/********************************************
+*            Just for debuging              *
+*********************************************/
+class DumpHTTPRequestToFile {
+	public function execute($targetFile) {
+		$data = sprintf(
+			"%s %s %s\n\nHTTP headers:\n",
+			$_SERVER['REQUEST_METHOD'],
+			$_SERVER['REQUEST_URI'],
+			$_SERVER['SERVER_PROTOCOL']
+		);
+
+		foreach ($this->getHeaderList() as $name => $value) {
+			$data .= $name . ': ' . $value . "\n";
+		}
+
+		$data .= "\nRequest body:\n";
+        
+		file_put_contents(
+			$targetFile,
+			$data . file_get_contents('php://input') . "\n", FILE_APPEND
+		);
+		
+		file_put_contents(
+			$targetFile,
+			$data . print_r($_REQUEST,true) . "\n*************************************************************\n", FILE_APPEND
+		);
+		
+
+        
+		echo("Done!\n\n");
+	}
+
+	private function getHeaderList() {
+		$headerList = [];
+		foreach ($_SERVER as $name => $value) {
+			if (preg_match('/^HTTP_/',$name)) {
+				// convert HTTP_HEADER_NAME to Header-Name
+				$name = strtr(substr($name,5),'_',' ');
+				$name = ucwords(strtolower($name));
+				$name = strtr($name,' ','-');
+
+				// add to list
+				$headerList[$name] = $value;
+			}
+		}
+
+		return $headerList;
+	}
+}
+
+
